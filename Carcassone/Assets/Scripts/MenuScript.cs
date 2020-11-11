221d6432e4ed8dba9aa34e9c3b0db8074e7231b9
blox-dev <bloxorz51gaming@gmail.com>
2020-11-11 19:28:05 +0300

    Finished temp menu screen

--- Carcassone/Assets/Scripts/MenuScript.cs
+++ Carcassone/Assets/Scripts/MenuScript.cs
@@ -7,7 +7,10 @@ public class MenuScript : MonoBehaviour
 {
     public Canvas canvas;
     private GameObject createGameButton, joinGameButton, quitGameButton;
-    private GameObject roomNameText, roomNameInput, roomNameErrorText, startGameButton;
+    private GameObject joinGameText, joinGameInput, joinGameErrorText, joinGameStartButton, joinGameToMenuButton;
+    private GameObject roomNameText, roomNameInput, roomNameErrorText, startGameButton, createGameToMenuButton;
+
+    private GameObject quitGameText, quitGameConfirmButton, quitGameCancelButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,45 @@ public class MenuScript : MonoBehaviour
         roomNameInput = canvas.transform.Find("roomNameInput").gameObject;
         roomNameErrorText = canvas.transform.Find("roomNameErrorText").gameObject;
         startGameButton = canvas.transform.Find("startGameButton").gameObject;
+        createGameToMenuButton = canvas.transform.Find("createGameToMenuButton").gameObject;
+
+        joinGameText = canvas.transform.Find("joinGameText").gameObject;
+        joinGameInput = canvas.transform.Find("joinGameInput").gameObject;
+        joinGameErrorText = canvas.transform.Find("joinGameErrorText").gameObject;
+        joinGameStartButton = canvas.transform.Find("joinGameStartButton").gameObject;
+        joinGameToMenuButton = canvas.transform.Find("joinGameToMenuButton").gameObject;
+
+        quitGameText = canvas.transform.Find("quitGameText").gameObject;
+        quitGameConfirmButton = canvas.transform.Find("quitGameConfirmButton").gameObject;
+        quitGameCancelButton = canvas.transform.Find("quitGameCancelButton").gameObject;
+
+        onMenuEnter();
+    }
+
+    public void onMenuEnter()
+    {
+        roomNameText.SetActive(false);
+        roomNameInput.SetActive(false);
+        roomNameErrorText.SetActive(false);
+        startGameButton.SetActive(false);
+        createGameToMenuButton.SetActive(false);
+
+
+        joinGameText.SetActive(false);
+        joinGameInput.SetActive(false);
+        joinGameErrorText.SetActive(false);
+        joinGameStartButton.SetActive(false);
+        joinGameToMenuButton.SetActive(false);
+
+
+        quitGameText.SetActive(false);
+        quitGameConfirmButton.SetActive(false);
+        quitGameCancelButton.SetActive(false);
+
+
+        createGameButton.SetActive(true);
+        joinGameButton.SetActive(true);
+        quitGameButton.SetActive(true);
     }
 
     public void onCreateGameButtonPress()
@@ -30,6 +72,7 @@ public class MenuScript : MonoBehaviour
         roomNameText.SetActive(true);
         roomNameInput.SetActive(true);
         roomNameErrorText.SetActive(true);
+        createGameToMenuButton.SetActive(true);
         startGameButton.SetActive(false);
     }
 
@@ -57,5 +100,65 @@ public class MenuScript : MonoBehaviour
     public void onStartGameButtonPress()
     {
         Debug.Log("let's go");
+    }
+
+    public void onBackToMenuButtonPress()
+    {
+        onMenuEnter();
+    }
+
+    public void onJoinGameButtonPress()
+    {
+        createGameButton.SetActive(false);
+        joinGameButton.SetActive(false);
+        quitGameButton.SetActive(false);
+
+        joinGameText.SetActive(true);
+        joinGameInput.SetActive(true);
+        joinGameErrorText.SetActive(true);
+        joinGameStartButton.SetActive(false);
+        joinGameToMenuButton.SetActive(true);
+    }
+
+    public void onJoinRoomNameChange()
+    {
+        InputField inputValue = joinGameInput.GetComponent<InputField>();
+        Text errorText = joinGameErrorText.GetComponent<Text>();
+        if (inputValue.text.Length >= 16 || inputValue.text.Length <= 2)
+            errorText.text = "ROOM NAME MUST HAVE 3-16 CHARACTERS";
+        else errorText.text = "";
+    }
+
+    public void onJoinRoomNameValidationFail()
+    {
+        InputField inputValue = joinGameInput.GetComponent<InputField>();
+        Text errorText = joinGameErrorText.GetComponent<Text>();
+        if (errorText.text != "")
+        {
+            inputValue.text = "";
+            joinGameStartButton.SetActive(false);
+        }
+        else joinGameStartButton.SetActive(true);
+    }
+
+    public void onJoinGameStartButtonPress()
+    {
+        Debug.Log("let's go");
+    }
+
+    public void onQuitGameButtonPress()
+    {
+        createGameButton.SetActive(false);
+        joinGameButton.SetActive(false);
+        quitGameButton.SetActive(false);
+
+        quitGameText.SetActive(true);
+        quitGameConfirmButton.SetActive(true);
+        quitGameCancelButton.SetActive(true);
+    }
+
+    public void onQuitGameConfirmButtonPress()
+    {
+        Application.Quit(0);
     }
 }

e04e75a85d126d31ae5d7d32a810ac121922af7b
firststef <firstbossspike@gmail.com>
2020-11-11 22:48:10 +0200

    Changed tiles and added instantiate method

---
+++ Carcassone/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Root;
+    public GameObject TilePrefab;
+
+    void Start()
+    {
+        CreateTile(5);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreateTile(uint tile){
+        var clone = Instantiate(TilePrefab);
+        var spriteName = "tile" + tile.ToString();
+        var sprite = Resources.Load<Sprite>("Images/Tiles/" + spriteName);
+        if (sprite)
+        {
+            var rend = clone.transform.GetChild(0).GetComponent<SpriteRenderer>();
+            rend.sprite = sprite;
+            Instantiate(clone, Root.GetComponent<Transform>());
+        }
+        else
+        {
+            Debug.Log("Failed to load" + "Images/Tiles/" + spriteName);
+        }
+        Destroy(clone);
+    }
+}
